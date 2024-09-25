9903bf916ff6418f6c63f098aacce85df2a468cd
Isaac Denny <isaac_denny@icloud.com>
2024-09-25 08:42:35 -0400

    - Include full library - Basic main program flow

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,13 @@
 
 int main(int argc, char *argv[])
 {
+    /*
+        1. Define algorithm result data format (what do we need to calculate)
+        2. Read datasets into memory
+        3. Measure performance and record data into results array
+        4. Display results array in multiple formats (console data dump, console graphs?, SDL2 graphs/visuals) 
+    */
+
     int data[4] = {0, 1, 2, 3};
     displayData(data);
     return 0;
