1961e72d10f1600141c12e488ff4fbe592c8d651
Isaac Denny <isaac_denny@icloud.com>
2024-09-26 10:41:14 -0400

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
 #include "sorts.h"
-#include "displayData.h"
-#include <map>
 
 int main(int argc, char *argv[])
 {
@@ -10,26 +8,26 @@ int main(int argc, char *argv[])
         3. Measure performance and record data into results array
         4. Display results array in multiple formats (console data dump, console graphs?, SDL2 graphs/visuals) 
     */
-    char* sortNames[7] = { "Selection", "Insertion", "Exchange", "Quick", "Merge", "Heap", "Bubble" };
-    SortMeasurement* sm;
-    SortMeasurement data[10];
+    // char* sortNames[7] = { "Selection", "Insertion", "Exchange", "Quick", "Merge", "Heap", "Bubble" };
+    // SortMeasurement* sm;
+    // SortMeasurement data[10];
     
-    for (int i = 0; i < 10; i++) {
-        sm = new SortMeasurement;
-        sm->sortName = sortNames[0];
-        sm->inputSize = (i + 1) * 20;
-        sm->time = i * 70;
-        sm->compares = 50 * 49 / 2;
-        sm->swaps = 50 * 49 / 2 + i + 21;
-        sm->type = RANDOM;
+    // for (int i = 0; i < 10; i++) {
+    //     sm = new SortMeasurement;
+    //     sm->sortName = sortNames[0];
+    //     sm->inputSize = (i + 1) * 20;
+    //     sm->time = i * 70;
+    //     sm->compares = 50 * 49 / 2;
+    //     sm->swaps = 50 * 49 / 2 + i + 21;
+    //     sm->type = RANDOM;
 
-        data[i] = *sm;
-    }
+    //     data[i] = *sm;
+    // }
 
-    for (int i = 0; i < 10; i++) {
-        std::cout << "Data " << i << " : " << data[i].inputSize << std::endl;
-    }
+    // for (int i = 0; i < 10; i++) {
+    //     std::cout << "Data " << i << " : " << data[i].inputSize << std::endl;
+    // }
 
-    displayData(data, 10);
+    // displayData(data, 10);
     return 0;
 }
