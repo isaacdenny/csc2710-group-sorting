4951b71152fad21542b00f675a614a339c8374b7
Graham McCabe <grahamamccabe@gmail.com>
2024-09-26 10:43:58 -0400

    fix

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Compile the SDL2 game source code
-g++ ./src/*.cpp -o output \
-    -I./libs/SDL2-2.30.7/include \
-    -L./libs/SDL2-2.30.7/lib/x86 \
-    -lSDL2main -lSDL2
+g++ ./src/*.cpp -o output
 
 # Check if the compilation was successful
 if [ $? -eq 0 ]; then
