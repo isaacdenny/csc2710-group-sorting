2a3859a88d422059da973b6303d0c774f315d877
Isaac Denny <isaac_denny@icloud.com>
2024-09-30 00:23:50 -0400

    organized sorts and started measurement example

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,11 @@
 #include "arraycopy.h"
 
 #include <iostream>
+#include <chrono>
+
+using namespace std::chrono;
+
+SortMeasurement measureSelection(std::string filename, int dataSize, dataSetType dataType);
 
 int main(int argc, char *argv[])
 {
@@ -9,39 +14,63 @@ int main(int argc, char *argv[])
         1. Define algorithm result data format (what do we need to calculate)
         2. Read datasets into memory
         3. Measure performance and record data into results array
-        4. Display results array in multiple formats (console data dump, console graphs?, SDL2 graphs/visuals) 
+        4. Display results array in multiple formats (console data dump, console graphs?, SDL2 graphs/visuals)
     */
-    // char* sortNames[7] = { "Selection", "Insertion", "Exchange", "Quick", "Merge", "Heap", "Bubble" };
-    // SortMeasurement* sm;
-    // SortMeasurement data[10];
-    
-    // for (int i = 0; i < 10; i++) {
-    //     sm = new SortMeasurement;
-    //     sm->sortName = sortNames[0];
-    //     sm->inputSize = (i + 1) * 20;
-    //     sm->time = i * 70;
-    //     sm->compares = 50 * 49 / 2;
-    //     sm->swaps = 50 * 49 / 2 + i + 21;
-    //     sm->type = RANDOM;
+    SortMeasurement sm = measureSelection("./data/duplicates/repeated_values-input-1000-float.txt", 1000, DUPLICATES);
 
-    //     data[i] = *sm;
-    // }
+    std::cout << sm.inputSize << std::endl;
+    std::cout << sm.sortType << std::endl;
+    std::cout << sm.compares << std::endl;
+    std::cout << sm.swaps << std::endl;
+    std::cout << sm.time << std::endl;
 
-    // for (int i = 0; i < 10; i++) {
-    //     std::cout << "Data " << i << " : " << data[i].inputSize << std::endl;
-    // }
-
-    // displayData(data, 10);
-
-
-
- /* This is the array copy function, here's how to get it to work.
-    int arraysize = 1000; // Change size
-    int arr[arraysize]; //Change array name
-    std::string filename = "/Users/grahammccabe/csc2710-group-sorting/data/duplicates/repeated_values-input-1000-float.txt"; // File name here
-
-    arraycopy(filename,arr,arraysize); //change array name */
-
-
-    return 0; 
+    return 0;
 }
+
+/*
+    Packages all of our functionality so far into a simple function for selection sort.
+
+    @param filename: the full or local path to the data file
+    @param dataSize: length of the data (integer)
+    @param dataType: type of data from the enum in sorts.h
+
+    @returns a SortMeasurement object with data about the sort performance
+*/
+SortMeasurement measureSelection(std::string filename, int dataSize, dataSetType dataType)
+{
+    if (dataSize <= 0)
+    {
+        throw std::invalid_argument("dataSize must be greater than zero");
+    }
+
+    int *data = new int[dataSize];
+    arraycopy(filename, data, dataSize);
+
+    SortMeasurement sm;
+    sm.sortType = SELECTION;
+    sm.dataType = dataType;
+    sm.inputSize = dataSize;
+
+    int compCount = 0, swapCount = 0;
+    auto start = high_resolution_clock::now();
+
+    selectionSort(data, dataSize, compCount, swapCount);
+
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(end - start).count();
+
+    sm.compares = compCount;
+    sm.swaps = swapCount;
+    sm.time = duration;
+
+    delete[] data;
+
+    return sm;
+}
+
+// measureHeap
+// measureMerge
+// measureQuick
+// measureExchange
+// measureBubble
+// measureInsertion
--- src/mergesort.cpp
+++ src/mergesort.cpp
@@ -1,25 +1,26 @@
+#include "sorts.h"
+
 /*
 Author: Kevin Schoeberle
 Mergesort
 */
 void merge(int size2, int size3, int array2[],
-                                 int array3[],
-                                 int array[], int* swapCount, int* compareCount
-                                 )
+           int array3[],
+           int array[], int &compareCount, int &swapCount)
 {
     int i, j, k;
     i = 0, j = 0, k = 0;
-    //variable index initializing
+    // variable index initializing
 
-    //loops while indexes are less than the sizes of the arrays
-    while(i < size2 && j < size3)
+    // loops while indexes are less than the sizes of the arrays
+    while (i < size2 && j < size3)
     {
 
-        //checks if the value at selected index is lower than the one at the other
-        //if it is then swaps OG array with the value of the lowest value between 
-        //the two compared
+        // checks if the value at selected index is lower than the one at the other
+        // if it is then swaps OG array with the value of the lowest value between
+        // the two compared
         compareCount++;
-        if(array2[i] < array3[j])
+        if (array2[i] < array3[j])
         {
             array[k] = array2[i];
             i++;
@@ -33,54 +34,50 @@ void merge(int size2, int size3, int array2[],
         swapCount++;
         k++;
     }
-    //checks if the index is still less than the size at the left array
-    if(i < size2)
+    // checks if the index is still less than the size at the left array
+    if (i < size2)
     {
-            while(i < size2)
-            {
+        while (i < size2)
+        {
             array[k] = array2[i];
             i++;
             k++;
             swapCount++;
-            }
+        }
     }
-    //checks if the index is still less than the size at the right array
+    // checks if the index is still less than the size at the right array
     else if (j < size3)
     {
-       while(j < size3)
-       {
-        array[k] = array3[j];
-        j++;
-        k++;    
-        swapCount++;
-       }
+        while (j < size3)
+        {
+            array[k] = array3[j];
+            j++;
+            k++;
+            swapCount++;
+        }
     }
 }
 
-
-
-//Mergesort main function
-void mergesort(int array[], int size, int* swapCount, int* compareCount)
+// Mergesort main function
+void mergesort(int array[], int size, int &compCount, int &swapCount)
 {
     if (size > 1)
     {
-        //sizes of arrays initializing divides the OG size by 2 and then floors it then subtracts it from OG size to get size3
-        int size2 = size/2;
+        // sizes of arrays initializing divides the OG size by 2 and then floors it then subtracts it from OG size to get size3
+        int size2 = size / 2;
         int size3 = size - size2;
-        
-        //initializes arrays at runtime
+
+        // initializes arrays at runtime
         int *array2 = new int[size2];
         int *array3 = new int[size3];
 
-
-        //the logic to Mergesort
-        mergesort(array2, size2, swapCount, compareCount);
-        mergesort(array3, size3, swapCount, compareCount);
-        merge(size2, size3, array2, array3, array, swapCount, compareCount);
-
+        // the logic to Mergesort
+        mergesort(array2, size2, compCount, swapCount);
+        mergesort(array3, size3, compCount, swapCount);
+        merge(size2, size3, array2, array3, array, compCount, swapCount);
 
         delete[] array2;
         delete[] array3;
-        //deletes the arrays made preventing memory leaks
+        // deletes the arrays made preventing memory leaks
     }
 }
